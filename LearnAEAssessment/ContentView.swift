a9e65d73c5b1ad65bf111b1f9dbaf1d9dffa4233
bakkertj <tbakker@gmail.com>
2020-11-21 17:20:54 -0600

    Initial Commit

---
+++ LearnAEAssessment/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  LearnAEAssessment
+//
+//  Created by Trevor Bakker on 11/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
